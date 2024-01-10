466a684d6ac6540f5a06162d8af83abaf23f63db
alexsandercoelho <a_pcoelho@hotmail.com>
2024-01-10 07:13:09 -0300

    Project created

---
+++ backend/src/test/java/com/apcoelhodev/appmovie/AppmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apcoelhodev.appmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
